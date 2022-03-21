d59bfe1673c0fda3bf5cc1cabe2b05d5aa856a63
annngure <annwanjikungure@gmail.com>
2022-03-21 11:42:23 +0300

    add index html

---
+++ hoodapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,re_path
+from . import views
+from django.contrib.auth.views import LoginView,LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    path('',views.index, name= "index")
+]
--- hoodapp/views.py
+++ hoodapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.decorators import login_required
+# from .forms import *
+from django.contrib import auth,messages
+from .models import *
 # Create your views here.
+def index(request):
+
+    return render(request,'index.html')
