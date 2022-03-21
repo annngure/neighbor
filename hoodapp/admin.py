a5de4f293b34f32bcc7154d7349b2abb6f269f99
annngure <annwanjikungure@gmail.com>
2022-03-21 14:36:16 +0300

    add models to admin

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -3,6 +3,6 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Business)
-admin.site.register(Neighborhood)
+admin.site.register(NeighbourHood)
 admin.site.register(Profile)
 admin.site.register(Posts)
